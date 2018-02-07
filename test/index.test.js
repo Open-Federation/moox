0b4bfc4da07bfdb72fd95b7b1d4fffd1d2ee1e23
suxiaoxin <hellosean1025@163.com>
2018-02-07 11:00:30 +0800

    feat: add test case

---
+++ test/index.test.js
@@ -0,0 +1,18 @@
+import test from 'ava'
+import moox from '../src/index'
+import user from './fixtures/user'
+
+test('moox', t=>{
+  let model = moox({
+    user
+  })
+  t.is(typeof model.getStore, 'function')
+  t.is(typeof model.user, 'object')
+  t.is(Object.keys(model.user).length, 6)
+
+  let store = model.getStore()
+
+  store.dispatch(model.user.addUserSyncAction())
+  let state = store.getState()
+  t.is(state.user.list.length, 3)
+})
